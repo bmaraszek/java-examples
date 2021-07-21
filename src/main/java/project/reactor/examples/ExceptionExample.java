b86c0959180648e991e47605c70f15d9bbff0f50
Bart Maraszek <bart.maraszek@imanage.com>
2021-07-21 01:19:47 +0100

    Error handling examples

--- src/main/java/project/reactor/examples/ExceptionExample.java
+++ src/main/java/project/reactor/examples/ExceptionExample.java
@@ -12,12 +12,60 @@ public class ExceptionExample {
   //  OnComplete() will never be sent. onError() will be sent instead.
   //  Any Exception will terminate a reactor stream.
   //
+  //  There are 2 categories of exception handling operators:
+  //    - Recover from Exception
+  //      - onErrorReturn(), onErrorResume(), onErrorContinue()
+  //    - Take an action on the exception and re-throw the exception (like try-catch)
+  //      - onErrorMap(), doOnError()
+  //
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
   public Flux<String> throwException() {
     return Flux.just("A", "B", "C")
         .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
         .concatWith(Flux.just("D"))
+        .log();
+  }
+
+  public Flux<String> onErrorReturn() {
+    return Flux.just("A", "B", "C")
+        .concatWith(Flux.error(new IllegalStateException("Exception Occurred")))
+        .onErrorReturn("Fallback value")
+        .log();
+  }
+
+  public Flux<String> onErrorResume(Exception exception) {
+
+    var recoveryFlux = Flux.just("D", "E", "F");
+
+    return Flux.just("A", "B", "C")
+        .concatWith(Flux.error(exception))
+        .onErrorResume((ex) -> {
+          log.error("Caught exception: ", ex);
+          if (ex instanceof IllegalStateException) {
+            return recoveryFlux;
+          } else {
+            return Flux.error(ex); // throw the exception back to the caller
+          }
+        })
+        .log();
+  }
+
+  public Flux<String> onErrorContinue() {
+
+    // drop the element causing the exception and continue emitting the remaining elems
+
+    return Flux.just("A", "B", "C")
+        .map(name -> {
+          if (name.equals("B")) {
+            throw new RuntimeException("Error!");
+          }
+          return name;
+        })
+        .onErrorContinue((ex, name) -> {
+          log.error("Exception is: ", ex);
+          log.info("Name is {}", name);
+        })
         .log();
   }
 }
--- src/test/java/project/reactor/examples/ExceptionExampleTest.java
+++ src/test/java/project/reactor/examples/ExceptionExampleTest.java
@@ -2,6 +2,7 @@ package project.reactor.examples;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 import utils.StaticAppender;
@@ -22,6 +23,7 @@ public class ExceptionExampleTest {
   }
 
   @Test
+  @DisplayName("Should throw exception")
   public void shouldThrowException() {
     var flux = subject.throwException();
 
@@ -30,5 +32,43 @@ public class ExceptionExampleTest {
         //        .expectError(RuntimeException.class);
         .expectErrorMatches(err -> err.getMessage().contains("Exception Occurred"))
         .verify();
+  }
+
+  @Test
+  @DisplayName("Should return a fallback value on error")
+  public void shouldReturnFallbackValueOnError() {
+    var flux = subject.onErrorReturn();
+
+    StepVerifier.create(flux)
+        .expectNext("A", "B", "C")
+        .expectNext("Fallback value")
+        .verifyComplete();
+  }
+
+  @Test
+  @DisplayName("Should return a fallback value on error resume")
+  public void shouldReturnFallbackValueOnErrorResume() {
+    var flux = subject.onErrorResume(new IllegalStateException("Illegal State!"));
+
+    StepVerifier.create(flux)
+        .expectNext("A", "B", "C", "D", "E", "F")
+        .verifyComplete();
+
+    flux = subject.onErrorResume(new RuntimeException("Runtime Exception!"));
+
+    StepVerifier.create(flux)
+        .expectNext("A", "B", "C")
+        .expectError(RuntimeException.class)
+        .verify();
+  }
+
+  @Test
+  @DisplayName("Should return a fallback value on error continue")
+  public void shouldReturnFallbackValueOnErrorContinue() {
+    var flux = subject.onErrorContinue();
+
+    StepVerifier.create(flux)
+        .expectNext("A", "C")
+        .verifyComplete();
   }
 }
