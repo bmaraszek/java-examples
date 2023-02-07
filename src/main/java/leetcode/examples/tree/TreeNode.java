ece0389cf64d864684b67edce6c607a6f11bbd60
Bart Maraszek <b.maraszek@gmail.com>
2023-02-07 20:53:34 +0000

    Add tree traversal examples

--- src/main/java/leetcode/examples/tree/TreeNode.java
+++ src/main/java/leetcode/examples/tree/TreeNode.java
@@ -75,4 +75,37 @@ public class TreeNode {
         root.right.left.right = new TreeNode(13);
         return root;
     }
+
+    public static TreeNode inOrderTree() {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+        return root;
+    }
+
+    public static TreeNode preOrderTree() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        return root;
+    }
+
+    public static TreeNode postOrderTree() {
+        TreeNode root = new TreeNode(7);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        return root;
+    }
 }
---
+++ src/main/java/leetcode/examples/tree/TreeTraversals.java
@@ -0,0 +1,140 @@
+package leetcode.examples.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                    //
+//  https://www.enjoyalgorithms.com/blog/iterative-binary-tree-traversals-using-stack                                  //
+//                                                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class TreeTraversals {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                                                //
+    // IN ORDER - from left to right                                                                                  //
+    //                                                                                                                //
+    //         4                                                                                                      //
+    //       /   \                                                                                                    //
+    //      2     6                                                                                                   //
+    //     / \   / \                                                                                                  //
+    //    1   3  5  7                                                                                                 //
+    //                                                                                                                //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public List<Integer> inOrder(TreeNode bst, List<Integer> list) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode current = bst;
+        while(!stack.isEmpty() || current != null) {
+            if(current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                list.add(current.data);
+                current = current.right;
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> inOrderRecursive(TreeNode bst, List<Integer> list) {
+        if(bst.left != null) inOrderRecursive(bst.left, list);
+        list.add(bst.data);
+        if(bst.right != null) inOrderRecursive(bst.right, list);
+        return list;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                                                //
+    // PRE ORDER - from root all the way left                                                                         //
+    //                                                                                                                //
+    //         1                                                                                                      //
+    //       /   \                                                                                                    //
+    //      2     5                                                                                                   //
+    //     / \   / \                                                                                                  //
+    //    3   4  6  7                                                                                                 //
+    //                                                                                                                //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public List<Integer> preOrder(TreeNode bst, List<Integer> list) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode current = bst;
+        TreeNode previous = null;
+        while(!stack.isEmpty() || current != null) {
+            if(current != null) {
+                list.add(current.data);
+                stack.push(current);
+                current = current.left;
+            } else {
+                previous = stack.pop();
+                current = previous.right;
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> preOrderRecursive(TreeNode bst, List<Integer> list) {
+        list.add(bst.data);
+        if(bst.left != null) preOrderRecursive(bst.left, list);
+        if(bst.right != null) preOrderRecursive(bst.right, list);
+        return list;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                                                //
+    // POST ORDER - children before the parent                                                                        //
+    //                                                                                                                //
+    //         7                                                                                                      //
+    //       /   \                                                                                                    //
+    //      3     6                                                                                                   //
+    //     / \   / \                                                                                                  //
+    //    1   2  4  5                                                                                                 //
+    //                                                                                                                //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public List<Integer> postOrder(TreeNode bst, List<Integer> list) {
+        LinkedList<TreeNode> mainStack = new LinkedList<>();
+        LinkedList<TreeNode> rightChildStack = new LinkedList<>();
+        TreeNode current = bst;
+        while(!mainStack.isEmpty() || current != null) {
+            if(current != null) {
+                if(current.right != null) rightChildStack.push(current.right);
+                mainStack.push(current);
+                current = current.left;
+            } else {
+                current = mainStack.peek();
+                if(!rightChildStack.isEmpty() && current.right == rightChildStack.peek()) {
+                    current = rightChildStack.pop();
+                } else {
+                    list.add(current.data);
+                    mainStack.pop();
+                    current = null;
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> postOrderRecursive(TreeNode bst, List<Integer> list) {
+        if(bst.left != null) postOrderRecursive(bst.left, list);
+        if(bst.right != null) postOrderRecursive(bst.right, list);
+        list.add(bst.data);
+        return list;
+    }
+
+
+    public static void main(String... args) {
+        TreeTraversals subject = new TreeTraversals();
+
+        System.out.println(subject.inOrder(TreeNode.inOrderTree(), new ArrayList<>()));
+        System.out.println(subject.inOrderRecursive(TreeNode.inOrderTree(), new ArrayList<>()));
+
+        System.out.println(subject.preOrder(TreeNode.preOrderTree(), new ArrayList<>()));
+        System.out.println(subject.preOrderRecursive(TreeNode.preOrderTree(), new ArrayList<>()));
+
+        System.out.println(subject.postOrder(TreeNode.postOrderTree(), new ArrayList<>()));
+        System.out.println(subject.postOrderRecursive(TreeNode.postOrderTree(), new ArrayList<>()));
+    }
+}
