ecb4be8222fba378b06da488e334a7d2b81a1607
Bart Maraszek <b.maraszek@gmail.com>
2023-02-11 20:23:10 +0000

    Add an anagram example

--- src/main/java/leetcode/examples/arraysAndHashing/Anagram.java
+++ src/main/java/leetcode/examples/arraysAndHashing/Anagram.java
@@ -3,6 +3,7 @@ package leetcode.examples.arraysAndHashing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                                                    //
 //  We'll use a hashmap to count the occurances of each letter and then compare them.                                 //
+//  Another approach is to sort the strings.                                                                          //
 //                                                                                                                    //
 //  Time complexity is O(A + B) - Where A and B are the lengths of the String                                         //
 //  Space complexity is O(A) - We allocate a map with a maximum of A characters in it                                 //
@@ -36,10 +38,20 @@ public class Anagram {
         return true;
     }
 
+    public static boolean isAnagramSort(String a, String b) {
+        char[] arrA = a.toCharArray();
+        char[] arrB = b.toCharArray();
+        Arrays.sort(arrA);
+        Arrays.sort(arrB);
+        return Arrays.compare(arrA, arrB) == 0;
+    }
+
     @Test
     @DisplayName("Strings of different lengths are not anagrams")
     void shouldReturnFalseForStringsOfDifferentLengths() {
         assertThat(isAnagram("a", "abc")).isFalse();
+
+        assertThat(isAnagramSort("a", "abc")).isFalse();
     }
 
     @Test
@@ -48,6 +60,10 @@ public class Anagram {
         assertThat(isAnagram("abc", "cba")).isTrue();
         assertThat(isAnagram("kajak", "kkjaa")).isTrue();
         assertThat(isAnagram("Luke Skywalker", "rekeL uklawykS")).isTrue();
+
+        assertThat(isAnagramSort("abc", "cba")).isTrue();
+        assertThat(isAnagramSort("kajak", "kkjaa")).isTrue();
+        assertThat(isAnagramSort("Luke Skywalker", "rekeL uklawykS")).isTrue();
     }
 
     @Test
@@ -56,5 +72,9 @@ public class Anagram {
         assertThat(isAnagram("abc", "bcd")).isFalse();
         assertThat(isAnagram("kajak", "kkajae")).isFalse();
         assertThat(isAnagram("Luke Skywalker", "rekel uklawyks")).isFalse();
+
+        assertThat(isAnagramSort("abc", "bcd")).isFalse();
+        assertThat(isAnagramSort("kajak", "kkajae")).isFalse();
+        assertThat(isAnagramSort("Luke Skywalker", "rekel uklawyks")).isFalse();
     }
 }
