2b0acc89ec14ca09931a02b4ad16cbf9f2f298e4
Bart Maraszek <b.maraszek@gmail.com>
2023-02-18 22:16:02 +0000

    Add a min stack example

---
+++ src/main/java/leetcode/examples/stack/MinStack.java
@@ -0,0 +1,55 @@
+package leetcode.examples.stack;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                    //
+//  Implement a MinStack class that supports push, pop, top, and min in constant time.                                //
+//                                                                                                                    //
+//  This can be done using 2 stacks. The minStack will hold the min value seen so far.                                //
+//  When we push a value into the stack, we also push into the min stack a value that is the min of                   //
+//  the current min and the top of the stack (minumum so far)                                                         //
+//                                                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public void push(int value) {
+        stack.push(value);
+        // min stack may be empty, so we need to check it
+        value = Math.min(value, minStack.isEmpty() ? value : minStack.peek());
+        minStack.push(value);
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    @Test
+    public void test() {
+        MinStack subject = new MinStack();
+        subject.push(-2);
+        subject.push(0);
+        subject.push(-3);
+        assertThat(subject.getMin()).isEqualTo(-3);
+        subject.pop();
+        assertThat(subject.top()).isEqualTo(0);
+        assertThat(subject.getMin()).isEqualTo(-2);
+    }
+}
