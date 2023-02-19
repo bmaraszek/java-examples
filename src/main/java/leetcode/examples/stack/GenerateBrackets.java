b62b186ab8820e9a5eae0c51e1417fa6c33fe081
Bart Maraszek <b.maraszek@gmail.com>
2023-02-19 20:31:59 +0000

    Add comments

--- src/main/java/leetcode/examples/stack/GenerateBrackets.java
+++ src/main/java/leetcode/examples/stack/GenerateBrackets.java
@@ -2,16 +2,15 @@ package leetcode.examples.stack;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                                                    //
-//  TODO: Study this example                                                                                          //
+//  TODO: Study this example
+//  TODO: Compate push(), pop() end stream() methods on Stack and Queue.
 //  NOTE: This is not really a stack exmaple. More like a queue.                                                      //
 //                                                                                                                    //
 //  Given n pairs of brackets, write a function to generate all combinations of well-formed bracket                   //
@@ -31,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public class GenerateBrackets {
 
-    private Stack<Character> stack = new Stack<>();
+    private Stack<String> stack = new Stack<>();
     private List<String> result = new ArrayList<>();
 
     public List<String> backtrack(int n) {
@@ -42,20 +41,16 @@ public class GenerateBrackets {
 
     public void backtrack(int openN, int closedN, int n) {
         if(openN == closedN && closedN == n) {
-            Iterator<Character> iter = stack.iterator();
-            String temp = "";
-            while(iter.hasNext()) {
-                temp = temp + iter.next();
-            }
+            String temp = stack.stream().collect(Collectors.joining());
             result.add(temp);
         }
         if(openN < n) {
-            stack.push('(');
+            stack.push("(");
             backtrack(openN + 1, closedN, n);
             stack.pop();
         }
         if(closedN < openN) {
-            stack.push(')');
+            stack.push(")");
             backtrack(openN, closedN + 1, n);
             stack.pop();
         }
